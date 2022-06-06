555166c3889b0ef3f6b0ca9046f6e681647f4cc7
Javier Nolasco <javnol@gmail.com>
2022-06-06 16:42:37 -0500

    3er commit: Inicio de dibujarFraccionPropia, falta completarla

--- Fraccion.php
+++ Fraccion.php
@@ -8,5 +8,13 @@
         public function exhibir(){
             echo $this->numerador . "/" . $this->denominador;
         }
+        // Devuelve true si la fracción es propia
+        public function espropia(){
+            // https://economipedia.com/definiciones/fracciones-propias.html
+            // Las fracciones propias son aquellas que tienen un numerador que es menor que el denominador. 
+            // Es decir, el número en la parte superior es menor que el número de la parte inferior
+            if( $this->numerador < $this->denominador ) return true;
+            else return false;
+        }
     }
 ?>
