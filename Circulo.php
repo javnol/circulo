c2f7d5012212aa932e7c460cea46230facc9a674
Javier Nolasco <javnol@gmail.com>
2022-06-06 16:31:44 -0500

    Segudo commit: Exhibe el círculo

---
+++ Circulo.php
@@ -0,0 +1,22 @@
+<?php
+    class Circulo {
+        public $radio, $centrox, $centro, $lado;
+        public function __construct($r, $cx, $cy, $l){
+            $this->radio = $r;
+            $this->centrox = $cx;
+            $this->centroy = $cy;
+            $this->lado = $l;
+        }
+        public function dibujar(){
+            $img = imagecreate($this->lado, $this->lado);
+            $negro = imagecolorallocate($img, 0, 0, 0);
+            $blanco = imagecolorallocate($img, 255, 255, 255);
+            $rojo = imagecolorallocate($img, 255, 0, 0);
+            //imagestring($im, 1, 5, 5,  "A Simple Text String", $color_texto);
+            imagefilledrectangle($img, 0, 0, $this->lado, $this->lado, $blanco);
+            imageellipse($img, $this->centrox, $this->centroy, $this->radio*2, $this->radio*2, $negro);    
+            imagepng($img);
+            imagedestroy($img);
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
-    require_once("Fraccion.php");
-    $frac = new Fraccion(10, 31);
-    echo "La fracción es:";
-    $frac->exhibir();
+    header("Content-Type: image/png");
+    // require_once("Fraccion.php");
+    // $frac = new Fraccion(10, 31);
+    // echo "La fracción es:";
+    // $frac->exhibir();
+    require_once("Circulo.php");
+    $cir = new Circulo(200, 200, 200, 400);
+    $cir->dibujar();
 ?>
